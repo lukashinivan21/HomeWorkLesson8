7995bc7fa77f7258dfecf8e8c8ce742a93aeb70b
Ivan <uvanes21@yandex.ru>
2022-01-28 09:01:22 +0300

    Добавлены методы toString, equals, hashCode в классы Book и Author.

--- src/Book.java
+++ src/Book.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Book {
     private final String name;
     private final Author author;
@@ -23,5 +25,23 @@ public class Book {
 
     public void setYearOfPublished(int yearOfPublished) {
         this.yearOfPublished = yearOfPublished;
+    }
+
+    @Override
+    public String toString() {
+        return "Полная информация о книге:\n1. Название: " + name + ".\n2. " + author + ".\n3. Год публикации: " + yearOfPublished + ".";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return yearOfPublished == book.yearOfPublished && Objects.equals(name, book.name) && Objects.equals(author, book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, author, yearOfPublished);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,7 @@ public class Main {
 
         Book warAndPeace = new Book("Война и мир", tolstoy, 1896);
         Book onegin = new Book("Евгений Онегин", pushkin, 1847);
+        Book onegin1 = new Book("Евгений Онегин", pushkin, 1847);
         Book tom = new Book("Приключения Тома Сойера", twain, 1859);
         Book uncle = new Book("Дядя Ваня", chekhov, 1882);
         Book deerSlayer = new Book("Зверобой", cooper, 1841);
@@ -27,5 +28,13 @@ public class Main {
                 System.out.println("Год переиздания книги " + arrayBook.getName() + " - " + arrayBook.getYearOfPublished());
             }
         }
+
+        System.out.println(tolstoy);
+        System.out.println(deerSlayer);
+        System.out.println(onegin.equals(tom));
+        System.out.println(onegin.hashCode());
+        System.out.println(onegin1.hashCode());
+
+
     }
 }
